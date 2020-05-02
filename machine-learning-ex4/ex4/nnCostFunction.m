a6a9da4712016c5117aa8d89eceacc89c28c14be
Rachit Malik <rachitmalik1947@gmail.com>
2020-05-02 22:31:59 +0530

    added regularization of gradient

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -98,7 +98,8 @@ Theta2_grad = (1/m) * Delta2;
 %               and Theta2_grad from Part 2.
 %
 
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda/m)* Theta1(:,2:end));
+Theta2_grad(:, 2:end) = Theta2_grad(:,2:end) + ((lambda/m) * Theta2(:, 2:end));
 
 
 
